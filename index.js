0552056117ac7d9da5bfbf3dd392a486d0a0d7b3
Gorbkos <gniewkoscielak@gmail.com>
2023-04-24 17:50:49 +0200

    s

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function changeBackground() {
   const body = document.getElementsByTagName("body")[0];
-  const button = document.getElementsByName("button")[0];
+  const button = document.getElementById("but");
   if (body.style.backgroundColor === "white") {
     body.style.backgroundColor = "#2e2e2e";
     button.style.backgroundColor = "white";
@@ -10,4 +10,23 @@ function changeBackground() {
     button.style.backgroundColor = "#2e2e2e";
     button.style.color = "white";
   }
+}
+function generateRandomColor() {
+  const color = document.getElementById("color");
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  color.style.backgroundColor = "#" + randomColor;
+  let button = document.getElementById("start").innerHTML;
+  if (button === "Start") {
+    document.getElementById("start").innerHTML = "Next";
+  }
+  const guesses = document.getElementsByClassName("guesses");
+  for (let i = 0; i < guesses.length; i++) {
+    const buttons = guesses[i].getElementsByTagName("button");
+    for (let j = 0; j < buttons.length; j++) {
+      const randomnumber = Math.floor(Math.random() * 16777215).toString(16);
+      const randomColor = "#" + randomnumber;
+      buttons[j].style.backgroundColor = randomColor;
+    }
+  }
+  return randomColor;
 }
