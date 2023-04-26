a6f4b7c368a051a98bc75a40f0cff074d15dd065
Gorbkos <gniewkoscielak@gmail.com>
2023-04-26 09:39:53 +0200

    a

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 function changeBackground() {
   const body = document.getElementsByTagName("body")[0];
   const button = document.getElementById("but");
+  const question = document.getElementById("question");
+
   if (body.style.backgroundColor === "white") {
     body.style.backgroundColor = "#2e2e2e";
     button.style.backgroundColor = "white";
     button.style.color = "#2e2e2e";
+    question.style.color = "white";
+    r;
   } else {
     body.style.backgroundColor = "white";
     button.style.backgroundColor = "#2e2e2e";
     button.style.color = "white";
+    question.style.color = "#2e2e2e";
   }
 }
 
 function generateRandomColor() {
+  const result = document.getElementById("result");
+  result.innerHTML = "";
   const color = document.getElementById("color");
   const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
   color.style.backgroundColor = randomColor;
@@ -21,17 +28,28 @@ function generateRandomColor() {
     document.getElementById("start").innerHTML = "Next";
   }
   const guesses = document.getElementsByClassName("guesses");
-  for (let i = 0; i < guesses.length; i++) {
-    const buttons = guesses[i].getElementsByTagName("button");
-    for (let j = 0; j < 3; j++) {
-      const randomnumber = Math.floor(Math.random() * 16777215).toString(16);
-      const randomColor2 = "#" + randomnumber;
-
-      let randomNum = Math.floor(Math.random() * (4 - 1 + 1) + 1);
-      document.getElementById("guess" + randomNum).innerHTML = randomColor;
+  const correct = Math.floor(Math.random() * 4) + 1;
+  for (let i = 1; i <= 4; i++) {
+    if (i === correct) {
+      document.getElementById("guess" + i).innerHTML = randomColor;
+    } else {
+      let randomColor2 =
+        "#" + Math.floor(Math.random() * 16777215).toString(16);
+      document.getElementById("guess" + i).innerHTML = randomColor2;
     }
   }
 
   console.log(randomColor);
   return randomColor;
+}
+
+function checkAnswer(guess) {
+  const color = document.getElementById("color").style.backgroundColor;
+  if (guess == color) {
+    document.getElementById("result").innerHTML = "Correct!";
+    document.getElementById("result").style.color = "green";
+  } else {
+    document.getElementById("result").innerHTML = "Wrong!";
+    document.getElementById("result").style.color = "red";
+  }
 }
