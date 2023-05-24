18c8c6a31c0fb55691cbd6471fbf3b96053a6889
Gorbkos <gniewkoscielak@gmail.com>
2023-05-24 09:28:53 +0200

    x

--- index.js
+++ index.js
@@ -16,40 +16,48 @@ function changeBackground() {
     question.style.color = "#2e2e2e";
   }
 }
+let GlobalRandomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
 
 function generateRandomColor() {
+  GlobalRandomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
   const result = document.getElementById("result");
   result.innerHTML = "";
+
   const color = document.getElementById("color");
-  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  color.style.backgroundColor = randomColor;
+
+  color.style.backgroundColor = GlobalRandomColor;
   let button = document.getElementById("start").innerHTML;
   if (button === "Start") {
     document.getElementById("start").innerHTML = "Next";
   }
-  const guesses = document.getElementsByClassName("guesses");
   const correct = Math.floor(Math.random() * 4) + 1;
   for (let i = 1; i <= 4; i++) {
     if (i === correct) {
-      document.getElementById("guess" + i).innerHTML = randomColor;
+      document.getElementById("guess" + i).innerHTML = GlobalRandomColor;
     } else {
       let randomColor2 =
         "#" + Math.floor(Math.random() * 16777215).toString(16);
       document.getElementById("guess" + i).innerHTML = randomColor2;
     }
   }
-  console.log("randomColor = " + randomColor);
-  return randomColor;
+  console.log(GlobalRandomColor);
 }
 
-function checkAnswer(guess, randomCol) {
-  console.log(randomCol);
+function checkAnswer(guess) {
+  console.log(GlobalRandomColor);
 
-  if (guess === randomCol) {
+  if (guess === GlobalRandomColor) {
     const result = document.getElementById("result");
     result.innerHTML = "Correct!";
+    result.style.color = "green";
   } else {
     const result = document.getElementById("result");
     result.innerHTML = "Wrong!";
+    result.style.color = "red";
+    result.classList.add("horizontal-shaking");
+    setTimeout(() => {
+      result.classList.remove("horizontal-shaking");
+    }, 1000);
   }
 }
